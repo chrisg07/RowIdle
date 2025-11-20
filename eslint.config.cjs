2532458589a924ec84762c25f60b49af0da87898
Christopher Guilliams <chrisg07@vt.edu>
2025-11-19 22:14:25 -0600

    added linting

---
+++ eslint.config.cjs
@@ -0,0 +1,38 @@
+const js = require("@eslint/js");
+const globals = require("globals");
+const tsPlugin = require("@typescript-eslint/eslint-plugin");
+const tsParser = require("@typescript-eslint/parser");
+
+module.exports = [
+  {
+    ignores: ["dist", "node_modules"],
+  },
+  {
+    ...js.configs.recommended,
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    },
+  },
+];
